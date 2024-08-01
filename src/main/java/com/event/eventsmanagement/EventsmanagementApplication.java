de06eedcc4534d478ddfa3dab3f627a0207bf6d6
Manoj Kumar Reddy Bhimavarapu <manojbh1999@gmail.com>
2024-08-01 18:49:02 +0530

    Removed filters package.
    Removed filters package, no security implement for this project.

--- src/main/java/com/event/eventsmanagement/EventsmanagementApplication.java
+++ src/main/java/com/event/eventsmanagement/EventsmanagementApplication.java
@@ -2,20 +2,10 @@ package com.event.eventsmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-
-import com.event.eventsmanagement.filters.CustomHeadersFilter;
 
 @SpringBootApplication
 public class EventsmanagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EventsmanagementApplication.class, args);
 	}
-	
-	FilterRegistrationBean<CustomHeadersFilter> customHeaderFilter() {
-        FilterRegistrationBean<CustomHeadersFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new CustomHeadersFilter());
-        registrationBean.addUrlPatterns("/api/v1/*");
-        return registrationBean;
-    }
 }
